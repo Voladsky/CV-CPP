0c3567976570f9baf2ad08f1ef0ebc8cabe651e1
Voladsky <krylov@sfedu.ru>
2025-10-02 21:05:17 +0300

    feat(image_processing): building a foundation for processing

--- include/image_processing.h
+++ include/image_processing.h
@@ -2,8 +2,9 @@
 #include <SDL3/SDL_opengl.h>
 #include "opencv2/opencv.hpp"
 #include <string>
+#include <vector>
 
-struct Image {
+struct ImageUnit {
     GLuint texture;
     cv::Mat mat;
     std::string filePath;
@@ -12,3 +13,13 @@ struct Image {
 namespace Processing {
     cv::Mat grayscale(const cv::Mat& input);
 }
+
+class ImageProcessor {
+public:
+    ImageProcessor();
+    void selectImage(int idx);
+    void loadImage(const std::string& path);
+private:
+    std::vector<ImageUnit> images;
+    int idx = -1;
+};
