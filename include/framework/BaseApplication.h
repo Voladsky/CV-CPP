2a0eec81efe83a81b315c83376ac04d6b82020e8
Voladsky <krylov@sfedu.ru>
2025-10-03 23:50:45 +0300

    refactor: removed boilerplate to BaseApplication

---
+++ include/framework/BaseApplication.h
@@ -0,0 +1,31 @@
+#ifndef _BASE_APPLICATION
+#define _BASE_APPLICATION
+
+#include <SDL3/SDL.h>
+#include <imgui.h>
+
+class BaseApplication
+{
+public:
+    void Initialize(const char *title, int width, int height);
+    void Run();
+    virtual ~BaseApplication() = default;
+
+protected:
+    virtual void Update(float delta_time) = 0;
+    virtual void RenderUI() = 0;
+    virtual void ProcessEvent(const SDL_Event &event) = 0;
+
+private:
+    SDL_Window *window_ = nullptr;
+    SDL_GLContext gl_context_ = nullptr;
+    ImGuiIO *io_ = nullptr;
+    float scale_ = 1.0f;
+    bool running_ = true;
+    void SetupImgui();
+    void NewFrame();
+    void Render();
+    void PollEvents();
+    void Shutdown();
+};
+#endif
---
+++ src/framework/BaseApplication.cpp
@@ -0,0 +1,175 @@
+#include "BaseApplication.h"
+#include <stdexcept>
+#include <imgui_impl_opengl3.h>
+#include <imgui_impl_sdl3.h>
+#include <SDL3/SDL_opengl.h>
+#include <SDL3/SDL_video.h>
+
+void BaseApplication::Initialize(const char *title, int width, int height)
+{
+    // Setup SDL
+    // [If using SDL_MAIN_USE_CALLBACKS: all code below until the main loop starts would likely be your SDL_AppInit() function]
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
+    {
+        throw std::runtime_error(SDL_GetError());
+    }
+
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100 (WebGL 1.0)
+    const char *glsl_version = "#version 100";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+#elif defined(IMGUI_IMPL_OPENGL_ES3)
+    // GL ES 3.0 + GLSL 300 es (WebGL 2.0)
+    const char *glsl_version = "#version 300 es";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+#elif defined(__APPLE__)
+    // GL 3.2 Core + GLSL 150
+    const char *glsl_version = "#version 150";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+#else
+    // GL 3.0 + GLSL 130
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+#endif
+
+    // Create window with graphics context
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    scale_ = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+    SDL_WindowFlags window_flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN | SDL_WINDOW_HIGH_PIXEL_DENSITY;
+    window_ = SDL_CreateWindow(title, (int)(width * scale_), (int)(height * scale_), window_flags);
+    if (window_ == nullptr)
+    {
+        throw std::runtime_error(SDL_GetError());
+    }
+    gl_context_ = SDL_GL_CreateContext(window_);
+    if (gl_context_ == nullptr)
+    {
+        throw std::runtime_error(SDL_GetError());
+    }
+
+    SDL_GL_MakeCurrent(window_, gl_context_);
+    SDL_GL_SetSwapInterval(1); // Enable vsync
+    SDL_SetWindowPosition(window_, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    SDL_ShowWindow(window_);
+
+    SetupImgui();
+}
+
+void BaseApplication::SetupImgui()
+{
+
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100 (WebGL 1.0)
+    const char *glsl_version = "#version 100";
+#elif defined(IMGUI_IMPL_OPENGL_ES3)
+    // GL ES 3.0 + GLSL 300 es (WebGL 2.0)
+    const char *glsl_version = "#version 300 es";
+#elif defined(__APPLE__)
+    // GL 3.2 Core + GLSL 150
+    const char *glsl_version = "#version 150";
+#else
+    // GL 3.0 + GLSL 130
+    const char *glsl_version = "#version 130";
+#endif
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    io_ = &ImGui::GetIO();
+    (void)io_;
+    io_->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io_->ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    // ImGui::StyleColorsLight();
+
+    // Setup scaling
+    ImGuiStyle &style = ImGui::GetStyle();
+    style.ScaleAllSizes(scale_); // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
+    style.FontScaleDpi = scale_; // Set initial font scale. (using io.ConfigDpiScaleFonts=true makes this unnecessary. We leave both here for documentation purpose)
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL3_InitForOpenGL(window_, gl_context_);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+}
+
+void BaseApplication::Run()
+{
+    while (running_)
+    {
+        auto frame_start = SDL_GetPerformanceCounter();
+
+        PollEvents();
+
+        NewFrame();
+
+        Update(io_->DeltaTime);
+        RenderUI();
+
+        Render();
+
+        auto frame_end = SDL_GetPerformanceCounter();
+    }
+    Shutdown();
+}
+
+void BaseApplication::NewFrame()
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
+}
+
+void BaseApplication::Render()
+{
+    ImGui::Render();
+    glViewport(0, 0, (int)io_->DisplaySize.x, (int)io_->DisplaySize.y);
+    glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    SDL_GL_SwapWindow(window_);
+}
+
+void BaseApplication::PollEvents()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        ImGui_ImplSDL3_ProcessEvent(&event);
+
+        if (event.type == SDL_EVENT_QUIT)
+            running_ = false;
+        if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED &&
+            event.window.windowID == SDL_GetWindowID(window_))
+            running_ = false;
+
+        // Forward to your specific event handling
+        ProcessEvent(event);
+    }
+}
+
+void BaseApplication::Shutdown()
+{
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DestroyContext(gl_context_);
+    SDL_DestroyWindow(window_);
+    SDL_Quit();
+}
